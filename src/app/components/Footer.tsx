0a3bd5409f01af5c6b9d05e6c8b422b4ae08a441
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-06 12:28:11 +0100

    Updated Footer.tsx
    Added Footer.styles.ts
    Installed styled-components and router-dom

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,11 @@
-import React, {FC} from 'react'
+import React, {FC} from 'react';
+import {FooterContainer} from './styles/Footer.styles';
 
 const Footer: FC = () => {
   return (
-    <div>Footer</div>
+    <FooterContainer>Coded by  
+        <a href='https://www.lunasmithart.com/' target='_blank' rel="noreferrer"> Luna Smith</a>, open source on <a href='https://github.com/Lu-Smith/my_todo_list_react_typescript_redux' target='_blank' rel="noreferrer">GitHub</a>.
+    </FooterContainer>
   )
 }
 
---
+++ src/app/components/styles/Footer.styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import {Link} from "react-router-dom";
+
+export const FooterContainer = styled.div`
+ background: #8ef6e4; 
+ color: black;
+ padding: 10px 0;
+ font-size: 20px;
+ line-height: 30px;
+
+ a {
+    color: #282A36;
+    text-decoration: none;
+    font-weight: 600;
+    transition: all 200ms ease-in-out;
+ }
+
+ a:hover {
+    color: #282A36;
+    border-bottom: 2px solid #282A36;
+ }
+`
+
